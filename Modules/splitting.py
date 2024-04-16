5635c8142b209b66d77a718b1c3052e713cdfceb
Nieeeb <william_nieboer@hotmail.com>
2024-04-16 14:23:46 +0200

    templates work now somehow

--- Modules/splitting.py
+++ Modules/splitting.py
@@ -1,10 +1,13 @@
 import cv2 as cv
 import os
+import numpy as np
+from imutils.object_detection import non_max_suppression 
+import glob
 
 def split_image(image):
     size = 500
     tiles_per_side = 5
-    cut_off_size = 5
+    cut_off_size = 0
     tile_size = size // tiles_per_side
     cut_images = []
     for i in range(tiles_per_side):
@@ -39,28 +42,73 @@ def generate_surf_features(image):
     working_image = convert_to_grayscale(working_image)
     sift = cv.SIFT_create()
     keypoints_sift, descriptors_sift = sift.detectAndCompute(working_image, None)
-    img_kp = cv.drawKeypoints(working_image, keypoints_sift, None, flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-    cv.imshow("Keypoints", img_kp)
+    return keypoints_sift, descriptors_sift
+
+def display_keypoints_on_image(image, keypoints):
+    img_kp = cv.drawKeypoints(image, keypoints, None, flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    cv.imshow(f"{image.size}", img_kp)
+
+def give_matching_boxes(templates, image):
+    boxes = []
+    for template in templates:
+        temp_gray = convert_to_grayscale(template)
+        image_gray = convert_to_grayscale(image)
+        result = cv.matchTemplate(image, template, cv.TM_CCOEFF_NORMED)
+        
+        threshold = 0.7
+        
+        (y_points, x_points) = np.where(result >= threshold)
+        
+        h, w, _ = template.shape
+        for x, y in zip(x_points, y_points):
+            boxes.append((x,y, x + w, y + h))
+            
+    boxes = non_max_suppression(np.array(boxes))
+    
+    return boxes
+
+def draw_boxes(image, boxes):
+    result = image.copy()
+    for (x1, y1, x2, y2) in boxes: 
+        # draw the bounding box on the image 
+        cv.rectangle(result, (x1, y1), (x2, y2), (255, 0, 0), 3)
+    return result
+
+def rotate_image(image):
+    rotated_images = [image, None, None, None]
+    
+    for i in range(3):
+        rotated_images[i+1] = cv.rotate(rotated_images[i], cv.ROTATE_90_CLOCKWISE)
+    
+    
+    return rotated_images
+
+def give_number_of_crowns(boxes):
+    return len(boxes)
 
 def main():
-    path = os.path.dirname(os.getcwd()) + '\King Domino dataset\Cropped and perspective corrected boards\\1.jpg'
-
+    #path = os.path.abspath(__file__ + '/../../../') + f'\King Domino dataset\Cropped and perspective corrected boards\\4.jpg'
+    #path = os.path.dirname(os.getcwd()) + '\King Domino dataset\Cropped and perspective corrected boards\\1.jpg'
+    path = r"King Domino dataset/Cropped and perspective corrected boards/32.jpg"
     image = cv.imread(path)
-    cv.imshow("Board", image)
-
+    #cv.imshow("Board", image)
     cut_images = split_image(image)
-    raw = cut_images[18]['cut_image']
-    cv.imshow("Raw", raw)
-    gray = convert_to_grayscale(raw)
-    cv.imshow("Converted", gray)
-    binary = binary_threshold_image(gray)
-    print(type(binary))
-    cv.imshow("Binary", binary)
-    median = median_filter(binary)
-    cv.imshow("Median", median)
-
-    generate_surf_features(image)
-
+    tile = cut_images[1]['cut_image']
+    
+    templates = [cv.imread(file) for file in glob.glob(r"Modules/Templates/*.png")]
+    
+    rotated = []
+    for template in templates:
+        rotated += rotate_image(template)
+        
+    boxes = give_matching_boxes(rotated, tile)
+    
+    drawn = draw_boxes(tile, boxes)
+    crowns_found = give_number_of_crowns(boxes)
+    print(f"Crowns Found: {crowns_found}")
+    # Show the template and the final output 
+    cv.imshow("After NMS", drawn) 
+    
     cv.waitKey()
     cv.destroyAllWindows()
     
