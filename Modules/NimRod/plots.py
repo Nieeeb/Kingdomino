2bfad2fa915c53dc693ecdcaa41abc60b1160a7f
Nieeeb <william_nieboer@hotmail.com>
2024-04-17 11:13:24 +0200

    labels og farver på plot

--- Modules/NimRod/plots.py
+++ Modules/NimRod/plots.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import pandas as pd
@@ -14,20 +15,24 @@ h = [i[0] for i in df['hsv']]
 s = [i[1] for i in df['hsv']]
 v = [i[2] for i in df['hsv']]
 
-labels = np.unique(df['label'])
-print(labels)
+color_labels = df['label'].unique()
+rgb_values = sns.color_palette("tab10", 8)
+color_map = dict(zip(color_labels, rgb_values))
+
+levels, categories = pd.factorize(df['label'])
+colors = [plt.cm.tab10(i) for i in levels]
+
+handles = [patches.Patch(color=plt.cm.tab10(i), label=c) for i, c in enumerate(categories)]
+
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-
-ax.scatter(data=df, xs=h, ys=s, zs=v, marker='o', c=pd.factorize(df['label'])[0])
-
-#df.plot.scatter(x='hsv',y='hsv',z='hsv')
+ax.scatter(h, s, v, c=df['label'].map(color_map))
 
 ax.set_xlabel('H')
 ax.set_ylabel('S')
 ax.set_zlabel('V')
 
-plt.legend(title="Tile Type")
+ax.legend(title='Tile Type', handles=handles)
 
 plt.show()
